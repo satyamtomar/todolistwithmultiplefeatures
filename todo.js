b211d936a717adf85f0c42b0909799c480b7b340
Satyam Tomar <saitamaitachi123@gmail.com>
2023-07-23 13:21:24 +0530

    satyam

--- todo.js
+++ todo.js
@@ -66,7 +66,7 @@ var subtasks=[];
     for (var i = 0; i < items.length; i++) {
       // Get the content of the <li>
       var value = items[i].querySelector('input');
-      subtasks.push(value.value);
+      subtasks.push({title:value.value,completed:false});
       // Output the value to the console
       console.log(value.value);
     }
@@ -76,11 +76,14 @@ var subtasks=[];
         alert("Cannot add an empty task!");
         return;
     }
-
-    tasks.push({title:task,completed:false,subtasks});
+    var dates=document.getElementById('datetime')
+    var dueDate = dates.value;
+    var priority=document.getElementById('priorityList').value;
+    tasks.push({title:task,completed:false,subtasks,dueDate,priority});
     subTaskList.innerHTML=''
     var subLI=document.createElement('li');
-
+    
+    
  var subIn = document.createElement('input');
  subIn.type = "subTasks";
  subIn.className = "subTasks";
@@ -107,6 +110,8 @@ editBtn.style.display='none';
     // Reconstruct the task display
     tasks.forEach((task, index) => {
         var li = document.createElement('li');
+        li.className=
+        'taskLI';
         var text = document.createElement('span');
     
     
@@ -129,57 +134,81 @@ editBtn.style.display='none';
 
 
         li.appendChild(text);
-  
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.className = "checkbox";
+       checkbox.checked= task.completed==true?true:false;
+       text.style.textDecoration = task.completed==true ?  'line-through':'none' ;
+
+        checkbox.onclick = 
+        function(event) {
+          toggleTask(event, task);
+        };
+       li.appendChild(checkbox);
         // li.appendChild(subtask);
         for (var i = 0; i < task.subtasks.length; i++) {
           console.log(task.subtasks[i],'sss');
               var subtask = document.createElement('span');
           
           
-              subtask.textContent = task.subtasks[i];
+              subtask.textContent = task.subtasks[i].title;
+              console.log(task.subtasks[i],'si');
               subtask.className = "subtask";
       
-              let checkbox = document.createElement('input');
-              checkbox.type = "checkbox";
-              checkbox.className = "checkbox";
-             checkbox.checked= task.completed==true?true:false;
-             text.style.textDecoration = task.completed==true ?  'line-through':'none' ;
-      
-              checkbox.onclick = 
-              function(event) {
-                toggleTask(event, task);
-              };
+      //         let checkbox = document.createElement('input');
+      //         checkbox.type = "checkbox";
+      //         checkbox.className = "checkbox";
+      //        checkbox.checked= task.subtasks[i].completed==true?true:false;
+      //        text.style.textDecoration = task.subtasks[i].completed==true ?  'line-through':'none' ;
+      // var a=i;
+      //         checkbox.onclick = 
+      //         function(event) {
+      //           toggleSubTask(event, task,a);
+      //           console.log(a,'ss');
+      //         };
               li.appendChild(subtask);
-              li.appendChild(checkbox);
+              // li.appendChild(checkbox);
         
             }
-            let checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            checkbox.className = "checkbox";
-           checkbox.checked= task.completed==true?true:false;
-           text.style.textDecoration = task.completed==true ?  'line-through':'none' ;
-    
-            checkbox.onclick = 
-            function(event) {
-              toggleTask(event, task);
-            };
-           li.appendChild(checkbox);
+          
+            var priority=document.createElement('span');
+            priority.className='priority';
+            priority.textContent=task.priority;
+            var dueDate=document.createElement('span');
+             dueDate.className='dueDate';
+             
+             dueDate.textContent=task.dueDate;
+          li.appendChild(priority);
+          li.appendChild(dueDate);   
          li.appendChild(editBtn);
         li.appendChild(removeBtn);
-  
+  li.style.backgroundColor=(task.priority=='lowPriority'?'rgb(106 221 222)':task.priority=='mediumPriority'?'#48acae':'#417074');
          taskList.appendChild(li);
     });
 }
 
 function toggleTask(e,myTask) {
     var task = e.target.previousSibling;
-    console.log(e.target,'sfasdsf');
+    console.log(myTask,'sfasdsf');
     myTask.completed=(myTask.completed==true?false: true);
     task.style.textDecoration = task.style.textDecoration === 'line-through' ? 'none' : 'line-through';
     localStorage.setItem('myList',JSON.stringify(tasks));
     // displayTasks();
 }
-
+// function toggleSubTask(e,myTask,i) {
+//   var task = e.target.previousSibling;
+//   console.log(e.target,'sfasdsf');
+//   for(let i=0;i<myTask.subtasks.length;i++)
+//   {
+//     myTask.subtasks[i].completed=(myTask.subtasks[i].completed==true?false: true);
+//     task.subtasks[i].style.textDecoration = task.subtasks[i].style.textDecoration === 'line-through' ? 'none' : 'line-through';
+      
+//   }
+//   // myTasks.completed=(myTask.completed==true?false: true);
+//   // task.style.textDecoration = task.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+//   localStorage.setItem('myList',JSON.stringify(tasks));
+//   // displayTasks();
+// }
 function editTask(event,index,task)
 {
   var input = document.getElementById('inputTask');
@@ -196,7 +225,46 @@ editBtn.style.display='block';
 // var editBtn=document.createElement('button');
 // editBtn.textContent = "Edit";
 // editBtn.id = "editTask";
+var subTaskList = document.getElementById('subTaskList');
+// var subtasks=[];
+subTaskList.style.display='none';
+    // Get all <li> children
+    // var items = subTaskList.children;
+  
+    // Loop through each <li>
+    // for (var i = 0; i < items.length; i++) {
+    //   // Get the content of the <li>
+    //   var value = items[i].querySelector('input');
+    //   subtasks.push({title:value.value,completed:false});
+    //   // Output the value to the console
+    //   console.log(value.value);
+    // }
+    // console.log(subtasks,
+    //   'subtsfsfa');
+    // if (!task.trim()) {
+    //     alert("Cannot add an empty task!");
+    //     return;
+    // }
+    var dates=document.getElementById('datetime')
+    dates.value=task.dueDate;
 
+    var priority=document.getElementById('priorityList');
+      priority.value=task.priority;
+      var moreSubTaskButton=document.getElementById('addMoreSubTask');
+      moreSubTaskButton.style.display='none'
+    // tasks.push({title:task,completed:false,subtasks,dueDate,priority});
+    // subTaskList.innerHTML=''
+    // var subLI=document.createElement('li');
+    
+    
+//  var subIn = document.createElement('input');
+//  subIn.type = "subTasks";
+//  subIn.className = "subTasks";
+       
+//         subLI.appendChild(subIn);
+//         subTaskList.appendChild(subLI);
+
+   
 editBtn.onclick = function(e) { finalEditTask(e,task,index); };
 
   
@@ -213,8 +281,12 @@ function finalEditTask(e,task,index)
 {
   var input = document.getElementById('inputTask');
   task.title=input.value;
-  
+  var due = document.getElementById('datetime');
+  task.dueDate=due.value;
+  var priorityList = document.getElementById('priorityList');
+  task.priority=priorityList.value;
   input.value = '';
+  due.value='';
   var addBtn=document.getElementById('addTask');
   addBtn.style.display='block';
   var editBtn=document.getElementById('editTask');
